1ae1392d5bfb2585379204e075425f146731c2e7
Masturffect <ianseal10@gmail.com>
2021-04-03 21:33:18 -0500

    Added functionality to search

--- census-project/src/App.js
+++ census-project/src/App.js
@@ -6,17 +6,31 @@ export default function App() {
 
   const [data, setData] = useState([])
   const [query, setQuery] = useState("")
+
+
   useEffect(() => {
     fetch("https://api.census.gov/data/2016/pep/population?get=POP,GEONAME&for=place:*&key=f4de31756c3fa31aab100b0cf2a657e05eaa40a3")
     .then(response => response.json())
     .then((json) => setData(json));
   }, []);
-  console.log(data[0]);
+
+  const col = (arr, n) => arr.map(x => x[n]);
+  const geoname = col(data.slice(1), 1);
+
+  function search(rows) {
+    const columns = rows[0] && Object.keys(rows[0]);
+    return rows.filter((row) => columns.some(
+      (column) => row[column].toString().toLowerCase().indexOf(query.toLowerCase()) > -1)
+    );
+  }
+
   return (
     <div>
-      <div>filters</div>
       <div>
-        <CensusTable data = {data} />
+        <input type = "text" value = {query} onChange = {(e) => setQuery(e.target.value)}/>
+      </div>
+      <div>
+        <CensusTable data = {search(data)} />
       </div>
     </div>
   );
--- census-project/src/censustable/CensusTable.js
+++ census-project/src/censustable/CensusTable.js
@@ -1,19 +1,72 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { unstable_batchedUpdates } from 'react-dom';
 
 export default function CensusTable({data}){
-    const columns = data[0] && Object.keys(data[0]);
-    console.log();
+    const heading = [ "CityName", "State", "Population"]; 
+    const columns = heading && Object.keys(heading);
+
+    const col = (arr, n) => arr.map(x => x[n]);
+    const geoname = col(data.slice(1), 1);
+    const temp = col(data.slice(1), 0);
+
+    const population = getPopulation(temp);
+    //console.log(geoname);
+    const cities = getCities(geoname);
+    //console.log(cities);
+
+    const states = getStates(geoname);
+    //console.log(states)
+
+    const tdata = tableData(cities, states, population.slice(1));
+
+    function getPopulation(temp){
+        let newArr = [];
+        for(let i = 0; i < temp.length; i++){
+           newArr[i] = data[i][0];
+        }
+        return newArr;
+    }
+
+    function getCities(geoname){
+        let newArr = [];
+        for(let i = 0; i < geoname.length; i++){
+            let first = geoname[i].split(",")[0];
+            let last = first.lastIndexOf(" ");
+            first = first.substring(0,last);
+            newArr.push(first);
+        }
+        return newArr;
+    }
+
+    function getStates(geoname){
+        let newArr = [];
+        for(let i = 0; i < geoname.length; i++){
+            let state = geoname[i].split(",")[1];
+            newArr.push(state);
+        }
+        return newArr;
+    }
+    
+    function tableData(cities, states, population){
+        let newArr = []; 
+        for (let i = 0; i < population.length; i++){
+            newArr.push([cities[i],states[i],population[i]]);
+        }
+        return newArr;
+
+    }
     return(
         <table cellPadding={0} cellSpacing={0}>
             <thead>
                 <tr>
-                    {data[0] && columns.map((heading) => <th>{heading}</th>)}
+                    {heading && heading.map((heading) => <th>{heading}</th>)}
                 </tr>
             </thead>
             <tbody>
-                {data.map(row=> <tr>
+                {tdata.map(row=> <tr>
                     {columns.map(columns => <td>{row[columns]}</td>)}
                 </tr>)}
+                
             </tbody>
         </table>
     )
