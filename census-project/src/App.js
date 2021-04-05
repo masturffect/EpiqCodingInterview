e7b9fcb59fad6cf39a275403af891645e84f5fbc
Masturffect <ianseal10@gmail.com>
2021-04-05 00:55:17 -0500

    Attempting sorting

--- census-project/src/App.js
+++ census-project/src/App.js
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from 'react';
-import { unstable_batchedUpdates } from 'react-dom';
 import './App.css';
 import CensusTable from "./censustable/CensusTable";
 
@@ -8,7 +7,10 @@ export default function App() {
   const [data, setData] = useState([])
   const [query, setQuery] = useState("");
   const [query2, setQuery2] = useState("");
-
+  const minValue = 0;
+  const maxValue = 10000000;
+  const [query3, setQuery3] = useState(minValue);
+  const [query4, setQuery4] = useState(maxValue);
   
   useEffect(() => {
     fetch("https://api.census.gov/data/2016/pep/population?get=POP,GEONAME&for=place:*&key=f4de31756c3fa31aab100b0cf2a657e05eaa40a3")
@@ -30,7 +32,7 @@ export default function App() {
 
   const tdata = tableData(cities, states, population.slice(1));
 
-  const select_states = grabEachState(states);
+  const select_states = grabEachState(states.sort());
 
   function getPopulation(temp){
       let newArr = [];
@@ -44,8 +46,14 @@ export default function App() {
       let newArr = [];
       for(let i = 0; i < geoname.length; i++){
           let first = geoname[i].split(",")[0];
-          let last = first.lastIndexOf(" ");
-          first = first.substring(0,last);
+          /*let last = first.lastIndexOf(" ");
+          if(last === ){
+            newArr.push(first);
+          }
+          else{
+            first = first.substring(0,last);
+            newArr.push(first);
+          }*/
           newArr.push(first);
       }
       return newArr;
@@ -70,17 +78,17 @@ export default function App() {
       curr = states[count];
       if(prev == curr){
         prev = curr;
-        count++;
       }
 
       else{
         newArr.push(curr);
         prev = curr;
-        count++;
       }
+      count++;
     }
     return newArr;
   }
+
   
   function tableData(cities, states, population){
       let newArr = []; 
@@ -91,27 +99,43 @@ export default function App() {
 
   }
 
+
+
   function search(rows) {
     const columns = rows[0] && Object.keys(rows[0]);
     return rows.filter((row) => columns.some(
       (column) => row[column].toString().toLowerCase().indexOf(query.toLowerCase()) > -1)
-    );
+      &&
+      columns.some(
+        (column) => row[column].toString().toLowerCase().indexOf(query2.toLowerCase()) > -1)
+      &&
+        parseInt(row[2]) >= query3
+      &&
+        parseInt(row[2]) <= query4);
   }
+  
 
   return (
     <div>
-      <div>
+      <div id = "filters">
         <label>Search for City Name
           <input type = "text" value = {query} onChange = {(e) => setQuery(e.target.value)}/>
         </label>
         <label>Select a State:
           <select value = {query2} onChange = {(e) => setQuery2(e.target.value)}>
+          <option value = "none">Select Any State</option>
             {select_states.map(state => (
               <option key ={state} value={state}>
                 {state}
               </option>
             ))}
           </select>
+        </label>
+        <label>Minimum Population
+              <input type = "number" value = {query3} onChange = {(e) => setQuery3(e.target.value)}/>
+        </label>
+        <label>Maximum Population
+              <input type = "number" value = {query4} onChange = {(e) => setQuery4(e.target.value)}/>
         </label>
       </div>
       <div>
--- census-project/src/censustable/CensusTable.js
+++ census-project/src/censustable/CensusTable.js
@@ -1,27 +1,61 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { unstable_batchedUpdates } from 'react-dom';
 import './Census.css';
 
-export default function CensusTable({data}){
-    const heading = [ "CityName", "State", "Population"]; 
+export default function CensusTable({ data }) {
+    const heading = ["CityName", "State", "Population"];
     const columns = heading && Object.keys(heading);
+    const col = (arr, n) => arr.map(x => x[n]);
+   
+    
 
+    //const sortedData = data.sort();
+   
 
-    return(
-        <div id = "t-container">
+    //const citiesA = tempcities.sort();
+    //something to track if its asc or des sort
+    //something to sort individual column
+
+    /*sortBy(key) {
+        data.sort( (a, b) => a < b);
+    }*/
+
+    
+
+    function sortFunction(a, b) {
+        if (parseInt(a[2]) === parseInt(b[2])) {
+            return 0;
+        }
+        else {
+            return (parseInt(a[2]) < parseInt(b[2])) ? -1 : 1;
+        }
+    }
+    function callSort(){
+        alert("Button was Clicked")
+        console.log(data[2]);
+        data = data.sort(sortFunction)
+        
+    }
+
+    return (
+        <div id="t-container">
             <table cellPadding={3} cellSpacing={3}>
-            <thead>
-                <tr>
-                    {data[0] && heading.map((heading) => <th>{heading}</th>)}
-                </tr>
-            </thead>
-            <tbody>
-                {data.map(row=> <tr>
-                    {columns.map(columns => <td>{row[columns]}</td>)}
-                </tr>)}
-                
-            </tbody>
-        </table>
+                <thead>
+                    <tr id="header">
+                        <button onClick = {callSort}>
+                            Click Me
+                        </button>
+                        {heading.map((heading) => <th>{heading}</th>)}
+                    </tr>
+
+                </thead>
+                <tbody>
+                    {data.sort(sortFunction).map(row => <tr>
+                        {columns.map(columns => <td>{row[columns]}</td>)}
+                    </tr>)}
+
+                </tbody>
+            </table>
         </div>
     )
 }
